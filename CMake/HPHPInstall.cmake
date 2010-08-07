310751a000db52985bbc0bdd97360f8ad8a7cf9f
Hui Chen <usa.chen@gmail.com>
2010-08-07 11:27:24 +0400

    Implement installation - Part 1
    Summary:
      * install() is disabled in third_party packages' CMakeLists.txt
      * hphp and hphpi are installed to CMAKE_INSTALL_PREFIX/bin
      * bin, CMake and doc are installed to CMAKE_INSTALL_PREFIX/share/hphp
      * src/third_party, src/runtime, src/system and src/util are installed
        to CMAKE_INSTALL_PREFIX/share/hphp/src. Only headers are needed.
      * LICENSE.* and README.md are copied to CMAKE_INSTALL_PREFIX/share/hphp
      * "./configure --prefix=/path/to/install" to set CMAKE_INSTALL_PREFIX.
        Default install prefix is /usr/local

---
+++ CMake/HPHPInstall.cmake
@@ -0,0 +1,35 @@
+# install hphp and hphpi
+install(PROGRAMS src/hphp/hphp src/hphpi/hphpi
+	DESTINATION bin)
+
+# copy CMake and doc
+install(DIRECTORY CMake doc 
+	DESTINATION share/hphp)
+
+# install everything under bin except for bin/run.sh
+install(DIRECTORY bin 
+	DESTINATION share/hphp
+	PATTERN "run.sh" EXCLUDE)
+
+# bin/run.sh is installed executable
+install(PROGRAMS bin/run.sh
+	DESTINATION share/hphp/bin)
+
+# install src/ffi
+install(DIRECTORY src/ffi
+	DESTINATION share/hphp/src)
+
+# install headers from src/runtime src/system src/util src/third_party
+set(install_header_dirs runtime system util third_party)
+foreach(dir ${install_header_dirs})
+	file(GLOB_RECURSE header_files RELATIVE "${CMAKE_CURRENT_SOURCE_DIR}/src/" 
+          PATTERN "src/${dir}/*.h" PATTERN "src/${dir}/*.hpp" PATTERN "src/${dir}/*.hh" PATTERN "src/${dir}/*.inc")
+	foreach(item ${header_files})
+		GET_FILENAME_COMPONENT(filepath ${item} PATH)
+		install(FILES src/${item} DESTINATION share/hphp/src/${filepath})
+	endforeach()
+endforeach()
+
+# install license and readme files
+install(FILES LICENSE.PHP LICENSE.ZEND README.md 
+	DESTINATION share/hphp)
